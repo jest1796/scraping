d6c3a0142233c35442b3ee55afc25ed96e4a61f4
katou <katoukanehito@yahoo.co.jp>
2022-11-13 22:42:38 +0900

    残業などの文字列取得、表示成功

--- mynav_test.py
+++ mynav_test.py
@@ -17,7 +17,6 @@ driver.get('https://tenshoku.mynavi.jp/')
 
 
 def scrape(urls,names):
-# def scrape(names,urls):
 
     # 検索後のページでもアンケート用小ウインドウが１つまたは２つ開く場合があるので
     # ２秒待機後にクリックを２回行ってそれらを消す
@@ -26,27 +25,59 @@ def scrape(urls,names):
     sleep(2)
     pag.click(200,200)
 
+    # １ページ内の会社名をまとめて取得。
+    co_names = driver.find_elements(By.XPATH,'//h3[contains(@class,"cassetteRecruit")]')
 
-    co_names = driver.find_elements(By.XPATH,'//h3')
+    # 1ページ内の会社詳細へのリンクをまとめて取得
     co_links = driver.find_elements(By.XPATH,'//a[@class="js__ga--setCookieOccName"]')
 
+
+    # １ページ内の会社名を収納するための空リストを準備
     page_names = []
+
     for co_name in co_names:
         page_names.append(co_name.text)
-        # print(co_name.text + "\n")
+        print(co_name.text + "\n")       #取得できたかの確認のため表示
+        
+    # 既にリスト形式で取得した会社名 names に 新たなページで取得した page_names を加える
     names += page_names    
-    
+
+    # １ページ内の会社詳細へのリンクを収納するための空リストを準備
     page_urls = []
+
     for co_link in co_links:
         page_urls.append(co_link.get_attribute)
-        # print(co_link.get_attribute('href') + "\n")
-        detail_link = driver.find_element(By.XPATH,'//a[@class="js__ga--setCookieOccName"]')
-        driver.execute_script('arguments[0].click();', detail_link)
+        print(co_link.get_attribute('href') + "\n")
+        driver.execute_script('arguments[0].click();', co_link)
+
+        # 会社詳細を開くたびに新たなタブが開くので、変数handle_arrayにタブ操作のためのドライバを入れる
+        handle_array = driver.window_handles
+
+        # 一番最後に開いたタブ（取得したい会社の詳細ページ）を指定するため、handle_arrayの[-1]を指定
+        driver.switch_to.window(handle_array[-1])
+
+        sleep(5)
+        try:
+            work_late = driver.find_element(By.XPATH,'//div[contains(.,"固定残業") or contains(.,"みなし残業") or contains(.,"見込み残業")]')
+            print(work_late.text)
+            conpany_name = driver.find_element(By.XPATH,'//span[@class="companyName"]')
+            print(conpany_name.text + "　注意　　")
+        except:
+            print("なし")
+      
+        # 上記のtry except文で要素が取得できなかった場合でも会社詳細を検索出来ているかをチェックするため社名を表示
+        conpany_name = driver.find_element(By.XPATH,'//span[@class="companyName"]')
+        print(conpany_name.text)
+
+        # 検索結果一覧のページに戻るためにhandle_array[0]を指定
+        driver.switch_to.window(handle_array[0])
+
+    # 既にリスト形式で取得した会社詳細リンク url に 新たなページで取得した page_urls を加える   
     urls += page_urls
     sleep(3)
 
 
-    # 次ページへのリンクを取得　取得できない場合は最後のページなのでexcept以降の処理へ
+    # 次ページへのリンクを取得する。　　もし取得できない場合は最後のページなのでexcept以降の処理へ
     try:
         next_link = driver.find_element(By.XPATH,'(//li/a[@class ="iconFont--arrowLeft"])[2]')
         driver.execute_script('arguments[0].click();', next_link)
@@ -119,23 +150,3 @@ main()
 sleep(5)
 driver.quit()
 
-# //input[@class="topSearch__text"] 検索窓のXpath 
-
-# (//p[@class="result__num"]/em)[1]/text() 検索する求人票の数
-
-# //p[@class="main_title"] 会社名
-# //h3[@class="cassetteRecruit__name"] 会社名こちらかも？
-
-# //a[@class="link entry_click entry3"]　詳細へのリンク
-# //a[@class="js__ga--setCookieOccName"] 詳細へのリンクこちらかも？
-
-# //a[@class="iconFont--arrowLeft"] 次のページへのリンク
-# //li[@class="pager__next"]/a
-
-# print("\n" + "スクレイピングを実行しますか？　（Y / N)")
-# Y_N = input()
-# if Y_N=="y":
-#     print("スクレイピング実行\n"+"UNDER CONSTRUCTION")
-#     main()
-# else:
-#     print("中止します。")
